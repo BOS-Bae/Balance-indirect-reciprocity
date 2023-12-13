e5028eb812da1593af784730d9f9c05942e2aada
BOS-Bae <ice970712@gmail.com>
2023-12-13 20:10:46 +0900

    cod edited

--- network-simulation/frequency_ts.jl
+++ network-simulation/frequency_ts.jl
@@ -3,15 +3,16 @@ include("../Indirect_network.jl")
 using Random
 
 if (length(ARGS) < 3)
-    print("usage : ϵ t_f t_avg \n")
+    print("usage : ϵ n_check t_avg \n")
     exit(1)
 end
 #N_f = parse(Int64, ARGS[1])
 #N_arr = 4:N_f
 N=4
 ϵ = parse(Float64, ARGS[1])
-t_f = parse(Int64, ARGS[2])
+n_check = parse(Int64, ARGS[2])
 t_avg = parse(Int64, ARGS[3])
+t_f = n_check*t_avg
 
 initial_prob = 0.5
 
@@ -26,7 +27,9 @@ push!(balanced_list, [[1,-1,1,-1],[-1,1,-1,1],[1,-1,1,-1],[-1,1,-1,1]])
 push!(balanced_list, [[1,-1,-1,1],[-1,1,1,-1],[-1,1,1,-1],[1,-1,-1,1]])
 #if (N==4) σ_distri = [-2,0,6] end
 
-σ_distri = zeros(8,t_f)
+σ_distri = zeros(8,t_avg)
+σ_check = zeros(8,t_avg)
+σ_result = zeros(8,n_check)
 
 prob = 1.0 # for complete graph, p=1.0
 e_matrix = zeros(N, N)
@@ -41,14 +44,13 @@ num_triad = number_arr[2]
 #if n_idx == 1
 #    println(num_edge,"   ", num_triad)
 #end
-τ = 0
-τ_tmp = 0
-t = 0 
+τ = τ_tmp = t = 0
+n_count = 0
 while(true)
-    global τ, t, τ_tmp
+    global τ, t, τ_tmp, n_count
     τ_tmp = original_update(L4_rule, σ_matrix, e_matrix, N, τ, ϵ)
     # For random sequential update, use the function below :
-    #τ_tmp = random_sequential_update(L6_rule, σ_matrix, e_matrix, Edge_list, τ)
+    #τ_tmp = random_sequential_update(L4_rule, σ_matrix, e_matrix, Edge_list, τ, ϵ)
     τ = τ_tmp
     if (Check_fixation(σ_matrix, Edge_list, Triad_list, N, num_edge, num_triad) == true)
         t += 1
@@ -63,8 +65,16 @@ while(true)
                     end
                 end
             end
-            if ((count == check) && (t >= (t_f - t_avg +1)))
-                σ_distri[b] += 1
+            if ((count == check) && ((t % t_avg) != 0))
+                σ_distri[b, t % t_avg] = 1
+            end
+        end
+        if ((t < t_f) && (t % t_avg == 0))
+            n_count += 1
+            σ_check[:,:] = σ_distri[:,:]
+            σ_distri .= 0
+            for b in 1:8
+                σ_result[b, n_count] = sum(σ_check[b,:])/t_avg
             end
         end
     end
@@ -73,10 +83,11 @@ while(true)
     end
 end
 
-σ_distri /= t_avg
-
-for b in 1:8
-    println(σ_distri[b])
+for n in 1:n_check-1
+    for b in 1:8
+        print(σ_result[b, n], "  ")
     #println(N," ", σ_distri[b])
+    end
+    print("\n")
 end
-println("sum : ", sum(σ_distri))
+println("sum : ", sum(σ_result[:,n_check-1]))
