b01813fdabc8fb079bf1cc8e6665ccd096529e8e
BOS-Bae <ice970712@gmail.com>
2024-01-30 14:13:45 +0900

    other code edited

--- Eigenvector/N5_power_method/L6_f.cpp
+++ Eigenvector/N5_power_method/L6_f.cpp
@@ -13,13 +13,14 @@ using std::copy;
 
 const int N = 5;
 
-void idx_to_mat(int idx, int mat[][N]);
-int mat_to_idx(int mat[][N]);
+void idx_to_mat(unsigned long long idx, int mat[][N]);
+unsigned long long mat_to_idx(int mat[][N]);
+void balanced_idx(vector<unsigned long long>& bal_list);
 void L6_rule(int mat_f[][N], int o, int d, int r, int idx_err);
 void n_list_gen(int n_num, int n_list[][N]);
 
 int main(int argc, char* argv[]) {
-	int num_of_bal = 16;
+	int num_of_bal = (int)pow(2,(N-1));
 
 	if(argc<3){
    		printf("./L6_f err iter flip_idx\n");
@@ -72,7 +73,7 @@ int main(int argc, char* argv[]) {
 	std::random_device rd;
 	std::mt19937 gen(rd());
 
-	int num_matrix = pow(2,N*N);
+	unsigned long long num_matrix = pow(2,N*N);
 	
 	std::uniform_real_distribution<> distri(0.0,1.0);
 
@@ -119,57 +120,94 @@ int main(int argc, char* argv[]) {
 }
 
 
-void idx_to_mat(int idx, int mat[][N]){
-	int i,j,slice_row,slice_col,M_ij,s_ij;
-	for (i=0; i<N; i++){
-		slice_row = N*(N-1-i);
-		for (j=0; j<N; j++){
-			slice_col = N-j-1;
-			M_ij = idx >> slice_row >> slice_col & 1;
-			mat[i][j] = 2*M_ij-1;
+void balanced_idx(vector<unsigned long long>& bal_list){
+	int max_idx = (int)pow(2,N)-1;
+	int id_mat[max_idx][N] = {0,};
+	for (int i = 0; i < max_idx; i++) {
+	  int idx = i;
+		for (int j = 0; j < N; j++) {
+			int id = idx & 1;
+			id_mat[i][j] = id;
+			idx = idx >> 1;
+		}
+	}
+	int max_num = 0;
+	for (int i = 0; i < max_idx; i++) {
+		int mat[N][N] = {0,};
+		for (int x = 0; x < N; x++) {
+			for (int y = 0; y < N; y++) {
+				if (id_mat[i][x] == id_mat[i][y]) mat[x][y] = mat[y][x] = 1;
+				else mat[x][y] = mat[y][x] = -1;
+			}
+		}
+		unsigned long long bal_idx = mat_to_idx(mat);
+		if (i == 0) {
+			bal_list.push_back(bal_idx);
+			max_num += 1;
+		}
+		else {
+			int check = 0; int different = 0;
+			for (int k = 0; k < max_num; k++) {
+				check += 1;
+				if (bal_list[k] != bal_idx)	different += 1;
+			}
+			if (check == different) {
+				bal_list.push_back(bal_idx);
+				max_num += 1;
+			}
 		}
 	}
 }
 
-void n_list_gen(int n_num, int n_list[][N]){
-	int i,j,val,idx;
-	for (i=0; i<n_num; i++){
-		// The number of possible configurations of assessment eror is n_num.
-		val = i;
-		for (j=0; j<N; j++){
-			n_list[i][j] = val & 1;
-			val = val >> 1;
-		}
-	}
+
+void idx_to_mat(unsigned long long idx, int mat[][N]) {
+  for (int i = 0; i < N; i++) {
+    int slice_row = N * (N - 1 - i);
+    for (int j = 0; j < N; j++) {
+      int slice_col = N - j - 1;
+      int M_ij = idx >> slice_row >> slice_col & 1;  // [TODO] check this line
+      mat[i][j] = 2 * M_ij - 1;
+    }
+  }
 }
 
-void L4_rule(int mat_f[][N], int o, int d, int r, int idx_err){
-	// mat_f should be empty matrix whose size is N by N.
-	int i,j,val,result;
-	val = mat_f[o][d];
-	if (mat_f[d][r] == 1){
-		if (mat_f[o][d] == -1) val = mat_f[o][r];
-	}
-	else val = -mat_f[o][r];
-
-	mat_f[o][d] = idx_err == 0 ? val : -val;
+void n_list_gen(int n_num, int n_list[][N]) {
+  for (int i = 0; i < n_num; i++) {
+    // The number of possible configurations of assessment eror is n_num.
+    int val = i;
+    for (int j = 0; j < N; j++) {
+      n_list[i][j] = val & 1;
+      val = val >> 1;
+    }
+  }
 }
 
-void L6_rule(int mat_f[][N], int o, int d, int r, int idx_err){
-	// mat_f should be empty matrix whose size is N by N.
-	int i,j;
-	mat_f[o][d] = (idx_err == 0 ? mat_f[o][r]*mat_f[d][r] : -mat_f[o][r]*mat_f[d][r]);
+void L4_rule(int mat_f[][N], int o, int d, int r, int idx_err) {
+  // mat_f should be empty matrix whose size is N by N.
+  int val = mat_f[o][d];
+  if (mat_f[d][r] == 1) {
+    if (mat_f[o][d] == -1)
+      val = mat_f[o][r];
+  } else
+    val = -mat_f[o][r];
+
+  mat_f[o][d] = idx_err == 0 ? val : -val;
 }
 
-int mat_to_idx(int mat[][N]){
-	int i,j,idx,element,binary_num;
-	idx = binary_num = 0;
-	for (i=0; i<N; i++){
-		for (j=0; j<N; j++){
-			element = ((int)(mat[N-i-1][N-j-1]+1)/2);
-			idx += element*(int)pow(2,binary_num);
-			binary_num += 1;
-		}
-	}
-	return idx;
+void L6_rule(int mat_f[][N], int o, int d, int r, int idx_err) {
+  // mat_f should be empty matrix whose size is N by N.
+  mat_f[o][d] = (idx_err == 0 ? mat_f[o][r] * mat_f[d][r] : -mat_f[o][r] * mat_f[d][r]);
+}
+
+unsigned long long mat_to_idx(int mat[][N]) {
+  unsigned long long idx = 0, binary_num = 0;
+  idx = binary_num = 0;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int element = ((int) (mat[N - i - 1][N - j - 1] + 1) / 2);
+      idx += element * (int) pow(2, binary_num);
+      binary_num += 1;
+    }
+  }
+  return idx;
 }
